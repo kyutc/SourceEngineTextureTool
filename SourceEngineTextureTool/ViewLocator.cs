91f2803c5dde19cd7e0e293e02e102626c518a8e
ABViney <vineyadrian@gmail.com>
2024-02-07 20:58:44 -0500

    Update initial files to match current version

--- SourceEngineTextureTool/ViewLocator.cs
+++ SourceEngineTextureTool/ViewLocator.cs
@@ -3,28 +3,30 @@ using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using SourceEngineTextureTool.ViewModels;
 
-namespace SourceEngineTextureTool
+namespace SourceEngineTextureTool;
+
+public class ViewLocator : IDataTemplate
 {
-    public class ViewLocator : IDataTemplate
+    public Control? Build(object? data)
     {
-        public IControl Build(object data)
-        {
-            var name = data.GetType().FullName!.Replace("ViewModel", "View");
-            var type = Type.GetType(name);
+        if (data is null)
+            return null;
 
-            if (type != null)
-            {
-                return (Control)Activator.CreateInstance(type)!;
-            }
-            else
-            {
-                return new TextBlock { Text = "Not Found: " + name };
-            }
+        var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            var control = (Control)Activator.CreateInstance(type)!;
+            control.DataContext = data;
+            return control;
         }
 
-        public bool Match(object data)
-        {
-            return data is ViewModelBase;
-        }
+        return new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public bool Match(object? data)
+    {
+        return data is ViewModelBase;
     }
 }
--- SourceEngineTextureTool/Views/MainWindow.axaml.cs
+++ SourceEngineTextureTool/Views/MainWindow.axaml.cs
@@ -2,21 +2,12 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace SourceEngineTextureTool.Views
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
-        }
+namespace SourceEngineTextureTool.Views;
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
     }
 }
