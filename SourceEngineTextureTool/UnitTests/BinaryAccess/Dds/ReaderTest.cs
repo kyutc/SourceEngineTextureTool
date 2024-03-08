864b631539636b96d91e235b967a183c87108f76
Abraham May <ckr395@mocs.utc.edu>
2024-03-08 13:28:16 -0500

    Added a Test_InvalidFileExtension test that throws an exception when the file extension given is not ".dds"
    Added a Test_InvalidFileExtension test that throws an exception when the filepath for the dds file doesn't exist.
    
    Added a Test_ValidDdsFile test that reads a dds file if it returns a NonNull byte array.

--- SourceEngineTextureTool/UnitTests/BinaryAccess/Dds/ReaderTest.cs
+++ SourceEngineTextureTool/UnitTests/BinaryAccess/Dds/ReaderTest.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Legacy;
 using SourceEngineTextureTool.Services.BinaryAccess.Dds;
 
 namespace SourceEngineTextureTool.UnitTests.BinaryAccess.Dds;
@@ -18,6 +21,45 @@ public class ReaderTest
 
         //Assert
         Assert.Throws<Exception>(act);
-
     }
+
+    [Test]
+    public void Test_InvalidFileExtension()
+    {
+        //Arrange
+        string invalidExtension = @"C:\Users\Abraham\Desktop\Burger.png";
+        
+        //Act
+        TestDelegate act = () => Reader.FromFile(invalidExtension);
+        
+        //Assert
+        Assert.Throws<Exception>(act);
+    }
+
+    [Test]
+    public void Test_NonExistingFile()
+    {
+        //Arrange
+        string filePath = @"C:\Users\Abraham\Desktop\nonexistent.dds";
+
+        //Act
+        TestDelegate act = () => Reader.FromFile(filePath);
+        
+        //Assert
+        Assert.Throws<FileNotFoundException>(act);
+    }
+
+    [Test]
+    public void Test_ValidDdsFile()
+    {
+        //Arrange
+        string filePath = @"C:\Users\Abraham\Desktop\black.dds";
+
+        //Act
+        byte[] result = Reader.FromFile(filePath);
+
+        //Assert
+        ClassicAssert.NotNull(result);
+    }
+    
 }
