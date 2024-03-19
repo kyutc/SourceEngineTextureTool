51c5e34df51994cf79b33abfc2b4fb0f07def532
ABViney <vineyadrian@gmail.com>
2024-03-19 04:18:34 -0400

    Include ItemsRepeater package. Initial TextureWorkspace control.

---
+++ SourceEngineTextureTool/Views/TextureWorkspace.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SourceEngineTextureTool.Views;
+
+public partial class TextureWorkspace : UserControl
+{
+    public TextureWorkspace()
+    {
+        InitializeComponent();
+    }
+}
