21ae88f5c385f814fe98581c669a434861c5fda6
ABViney <vineyadrian@gmail.com>
2024-03-25 15:10:14 -0400

    Expose width and height properties of the Texture's Resolution to the view.

--- SourceEngineTextureTool/ViewModels/TextureViewModel.cs
+++ SourceEngineTextureTool/ViewModels/TextureViewModel.cs
@@ -13,6 +13,48 @@ public class TextureViewModel : ViewModelBase
 
     [Reactive] public Resolution TextureResolution { get; set; }
 
+    // Todo: Width and height should be modified at the same time. Impossible currently.
+    public int ResolutionWidth
+    {
+        get => TextureResolution.Width;
+        set => TryUpdateTextureResolution(value, ResolutionHeight);
+    }
+    
+    // Todo: Width and height should be modified at the same time. Impossible currently.
+    public int ResolutionHeight
+    {
+        get => TextureResolution.Height;
+        set => TryUpdateTextureResolution(ResolutionWidth, value);
+    }
+
+    /// <summary>
+    /// Attempt to assign a new <see cref="TextureResolution"/>
+    /// </summary>
+    /// <param name="width">A potentially invalid width value</param>
+    /// <param name="height">A potentially invalid height value</param>
+    /// <remarks>
+    /// The <see cref="Avalonia.Controls.NumericUpDown"/> control has a <code>decimal?</code> for it's value property.
+    /// A value is propagated every time the content of the control's text box changes (oddly, excluding when that value is 0 e.g.: 01 -> 0, unable to repeat in another project),
+    /// including when the content is empty/null. This throws an exception that breaks the application and generates ugly validation errors.
+    /// <see cref="https://github.com/AvaloniaUI/Avalonia/issues/10793"/>
+    /// Until a solution is implemented, this workaround attempts to set a new resolution. If an exception is thrown,
+    /// either null or an invalid value was provided, and nothing happens here.
+    /// </remarks>
+    public void TryUpdateTextureResolution(int? width, int? height)
+    {
+        if (width is not null && height is not null)
+        {
+            try
+            {
+                Resolution newResolution = new(width.Value, height.Value);
+                TextureResolution = newResolution;
+            }
+            catch (ArgumentException e)
+            {
+            }
+        }
+    }
+    
     [Reactive] public ushort FrameCount { get; set; }
 
     [Reactive] public int MipmapCount { get; set; }
@@ -38,6 +80,8 @@ public class TextureViewModel : ViewModelBase
             .Subscribe(textureResolution =>
             {
                 Texture.Resolution = textureResolution;
+                this.RaisePropertyChanged(nameof(ResolutionWidth));
+                this.RaisePropertyChanged(nameof(ResolutionHeight));
                 Refresh();
             });
 
