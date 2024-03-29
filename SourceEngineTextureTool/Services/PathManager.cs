dc0e7a3b5ffd34586c725083d0397f5339308047
Kenzy Young <zxc114@mocs.utc.edu>
2024-03-29 05:29:46 -0400

    Additional path methods

--- SourceEngineTextureTool/Services/PathManager.cs
+++ SourceEngineTextureTool/Services/PathManager.cs
@@ -51,29 +51,6 @@ public static class PathManager
         );
     }
 
-    public static string GetImageDirectory(ushort mipmap, ushort frame, ushort face, ushort slice)
-    {
-        return Path.Join(
-            BaseDir,
-            string.Format(Slug, mipmap, frame, face, slice)
-            );
-    }
-
-    /// <summary>
-    /// Gets a partially complete path with the frame remaining as a format string.
-    /// </summary>
-    /// <param name="mipmap"></param>
-    /// <param name="face"></param>
-    /// <param name="slice"></param>
-    /// <returns></returns>
-    public static string GetFrameFormatString(ushort mipmap, ushort face, ushort slice)
-    {
-        return Path.Join(
-            BaseDir,
-            string.Format(FrameSlug, mipmap, face, slice)
-        );
-    }
-
     /// <summary>
     /// Path of the normalised input image after being imported
     /// </summary>
@@ -86,6 +63,21 @@ public static class PathManager
     {
         return Path.Join(BaseDir,
             string.Format(Slug, mipmap, frame, face, slice),
+            "normalised.png"
+        );
+    }
+    
+    /// <summary>
+    /// Return a partial path with a format string for Frame.
+    /// </summary>
+    /// <param name="mipmap"></param>
+    /// <param name="face"></param>
+    /// <param name="slice"></param>
+    /// <returns></returns>
+    public static string ImportedImage(ushort mipmap, ushort face, ushort slice)
+    {
+        return Path.Join(BaseDir,
+            string.Format(FrameSlug, mipmap, face, slice),
             "normalised.png"
         );
     }
@@ -106,6 +98,14 @@ public static class PathManager
             "preview.png"
         );
     }
+    
+    public static string PreviewImage(ushort mipmap, ushort face, ushort slice)
+    {
+        return Path.Join(BaseDir,
+            string.Format(FrameSlug, mipmap, face, slice),
+            "preview.png"
+        );
+    }
 
     /// <summary>
     /// Path for the image which has been preprocessed (autocrop, compositing, scaling, etc.)
@@ -123,6 +123,14 @@ public static class PathManager
         );
     }
     
+    public static string PreprocessedImage(ushort mipmap, ushort face, ushort slice)
+    {
+        return Path.Join(BaseDir,
+            string.Format(FrameSlug, mipmap, face, slice),
+            "preprocessed.png"
+        );
+    }
+    
     /// <summary>
     /// Path for converted DDS output file
     /// </summary>
@@ -133,9 +141,16 @@ public static class PathManager
     /// <returns>Full path to converted DDS file</returns>
     public static string ConvertedImage(ushort mipmap, ushort frame, ushort face, ushort slice)
     {
-        // TODO: Consider multiple output mode for Crunch, will create complexities for path handling
         return Path.Join(BaseDir,
             string.Format(Slug, mipmap, frame, face, slice),
+            "converted.dds"
+        );
+    }
+    
+    public static string ConvertedImage(ushort mipmap, ushort face, ushort slice)
+    {
+        return Path.Join(BaseDir,
+            string.Format(FrameSlug, mipmap, face, slice),
             "converted.dds"
         );
     }
