984928469b17c202eaec70b088e26a80587f7166
Kenzy Young <zxc114@mocs.utc.edu>
2024-04-01 00:59:26 -0400

    Refactor frame format string conditional Instead of overloaded methods, frame can be nulled to tell the method we want a format string with frame as a placeholder for our use later.

--- SourceEngineTextureTool/Services/PathManager.cs
+++ SourceEngineTextureTool/Services/PathManager.cs
@@ -10,7 +10,7 @@ public static class PathManager
     // Assumption: mipmaps <= 99; frames <= 999, faces <= 9, slices <= 9
     // Though this shouldn't break at values above those, the path will just be less pretty
     private const string Slug = "mipmap_{0:D2}/frame_{1:D3}/face_{2:D1}/slice_{3:D1}";
-    private const string FrameSlug = "mipmap_{0:D2}/frame_{{0:D3}}/face_{1:D1}/slice_{2:D1}";
+    private const string FrameSlug = "mipmap_{0:D2}/frame_{{0:D3}}/face_{2:D1}/slice_{3:D1}";
 
     static PathManager()
     {
@@ -36,25 +36,11 @@ public static class PathManager
     /// <param name="face"></param>
     /// <param name="slice"></param>
     /// <returns>Full path to imported normalised PNG file</returns>
-    public static string ImportedImage(ushort mipmap, ushort frame, ushort face, ushort slice)
+    public static string ImportedImage(ushort mipmap, ushort? frame, ushort face, ushort slice)
     {
+        string slug = frame is null ? FrameSlug : Slug;
         return Path.Join(BaseDir,
-            string.Format(Slug, mipmap, frame, face, slice),
-            "normalised.png"
-        );
-    }
-    
-    /// <summary>
-    /// Return a partial path with a format string for Frame.
-    /// </summary>
-    /// <param name="mipmap"></param>
-    /// <param name="face"></param>
-    /// <param name="slice"></param>
-    /// <returns></returns>
-    public static string ImportedImage(ushort mipmap, ushort face, ushort slice)
-    {
-        return Path.Join(BaseDir,
-            string.Format(FrameSlug, mipmap, face, slice),
+            string.Format(slug, mipmap, frame, face, slice),
             "normalised.png"
         );
     }
@@ -68,18 +54,11 @@ public static class PathManager
     /// <param name="face"></param>
     /// <param name="slice"></param>
     /// <returns>Full path to preview PNG file</returns>
-    public static string PreviewImage(ushort mipmap, ushort frame, ushort face, ushort slice)
+    public static string PreviewImage(ushort mipmap, ushort? frame, ushort face, ushort slice)
     {
+        string slug = frame is null ? FrameSlug : Slug;
         return Path.Join(BaseDir,
-            string.Format(Slug, mipmap, frame, face, slice),
-            "preview.png"
-        );
-    }
-    
-    public static string PreviewImage(ushort mipmap, ushort face, ushort slice)
-    {
-        return Path.Join(BaseDir,
-            string.Format(FrameSlug, mipmap, face, slice),
+            string.Format(slug, mipmap, frame, face, slice),
             "preview.png"
         );
     }
@@ -92,18 +71,11 @@ public static class PathManager
     /// <param name="face"></param>
     /// <param name="slice"></param>
     /// <returns>Full path to converted DDS file</returns>
-    public static string ConvertedImage(ushort mipmap, ushort frame, ushort face, ushort slice)
+    public static string ConvertedImage(ushort mipmap, ushort? frame, ushort face, ushort slice)
     {
+        string slug = frame is null ? FrameSlug : Slug;
         return Path.Join(BaseDir,
-            string.Format(Slug, mipmap, frame, face, slice),
-            "converted.dds"
-        );
-    }
-    
-    public static string ConvertedImage(ushort mipmap, ushort face, ushort slice)
-    {
-        return Path.Join(BaseDir,
-            string.Format(FrameSlug, mipmap, face, slice),
+            string.Format(slug, mipmap, frame, face, slice),
             "converted.dds"
         );
     }
