e2a16e9d23b7e8679bcd6b791b14b6275a210a6f
Kenzy Young <zxc114@mocs.utc.edu>
2024-04-03 03:23:48 -0400

    Add additional necessary settings to the Vtf Settings model

--- SourceEngineTextureTool/Models/Settings/Vtf.cs
+++ SourceEngineTextureTool/Models/Settings/Vtf.cs
@@ -6,12 +6,21 @@ namespace SourceEngineTextureTool.Models.Settings;
 public class Vtf
 {
     // TODO: Validate supported version entered
-    public (uint Major, uint Minor) VtfVersion = (7, 1);
+    public (uint Major, uint Minor) Version = (7, 1);
     
     public ushort Width = 512;
     public ushort Height = 512;
 
     public Format FormatOption = Format.DXT1;
     
-    public uint FlagsOption = 0;
+    public uint Flags = 0;
+
+    public float BumpmapScale = 1.0f;
+
+    public ushort FirstFrame = 0;
+
+    public byte LowResWidth = 0;
+    public byte LowResHeight = 0;
+
+    public (float R, float G, float B) Reflectivity = (0.5f, 0.5f, 0.5f);
 }
